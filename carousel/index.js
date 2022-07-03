b5cb3d904d0cb0eb6a6c7d4e389c0a4c9d2918d5
Jake Samuels <samuels242@googlemail.com>
2022-07-03 11:42:16 +0100

    Automate slideshow

--- carousel/index.js
+++ carousel/index.js
@@ -1,28 +1,61 @@
 const slides = document.querySelectorAll(".carousel-item");
+const dots = document.querySelectorAll(".dot");
 const prevBtn = document.getElementById("carousel-btn-prev");
 const nextBtn = document.getElementById("carousel-btn-next");
 
 let slidePosition = 0;
 const totalSlides = slides.length;
 
-hideAllSlides = () => {
+// showSlides = () => {
+//   slides.forEach((slide) => (slide.style.display = "none"));
+//   slidePosition++;
+
+//   if (slidePosition > slides.length) {
+//     slidePosition = 1;
+//   }
+
+//   dots.forEach((dot) => dot.classList.remove("active"));
+
+//   slides[slidePosition - 1].style.display = "block";
+//   dots[slidePosition - 1].className += " active";
+//   setTimeout(showSlides, 2000);
+// };
+
+// AUTOMIC SLIDE
+switchClass = () => {
   slides.forEach((slide) => {
-    slide.classList.remove("carousel-item-visible");
     slide.classList.add("carousel-item-hidden");
+    slide.classList.remove("carousel-item-visible");
   });
+
+  dots.forEach((dot) => dot.classList.remove("active"));
 };
 
+// MANUAL SLIDE
+// hideAllSlides = () => {
+//   slides.forEach((slide) => {
+//     slide.classList.remove("carousel-item-visible");
+//     slide.classList.add("carousel-item-hidden");
+//   });
+// };
+
 moveToPrevSlide = () => {
-  hideAllSlides();
+  //   hideAllSlides();
+  switchClass();
   slidePosition === 0 ? (slidePosition = totalSlides - 1) : slidePosition--;
   slides[slidePosition].classList.add("carousel-item-visible");
+  dots[slidePosition].classList.add("active");
 };
 
 moveTonextSlide = () => {
-  hideAllSlides();
+  //   hideAllSlides();
+  switchClass();
   slidePosition === totalSlides - 1 ? (slidePosition = 0) : slidePosition++;
   slides[slidePosition].classList.add("carousel-item-visible");
+  dots[slidePosition].classList.add("active");
 };
 
 prevBtn.addEventListener("click", moveToPrevSlide);
 nextBtn.addEventListener("click", moveTonextSlide);
+
+setInterval(() => moveTonextSlide(), 5000);
